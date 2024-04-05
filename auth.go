6f947e4cdfa550265c9f22a8776864ca9be0d5e8
aa.rubtcov <aa.rubtcov@severstal.com>
2024-04-05 15:34:58 +0300

    add vault api

--- auth.go
+++ auth.go
@@ -1,6 +1,26 @@
 package auth
 
+import (
+	"log"
+
+	"github.com/hashicorp/vault/api"
+)
+
 // AddIntNum is to add two integer numbers
 func AddIntNum(num1, num2 int) int {
 	return num1 + num2
+}
+
+func tokenVaultClient(vaultAddr string, vaultToken string) *api.Client {
+
+	// Initialize a Vault API client
+	config := api.DefaultConfig()
+	config.Address = vaultAddr
+	client, err := api.NewClient(config)
+	if err != nil {
+		log.Fatalf("failed to create Vault client: %v", err)
+	}
+	client.SetToken(vaultToken)
+
+	return client
 }
